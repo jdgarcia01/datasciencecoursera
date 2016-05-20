2b7a97d2c767fb8be7ba732d7ecd16794897e99c
John Garcia <john.d.garcia01@gmail.com>
2016-05-20 12:44:52 -0500

    Added the complete function, not complete yet....not pun

--- assignment_1.R
+++ assignment_1.R
@@ -1,5 +1,39 @@
+complete <- function(directory, id = 1:332){
 
-
+  ## Read a directory of files and return a dataframe 
+  ## of the filename and number of complete observations 
+  ## in each file. 
+  files_to_read   <- list.files(directory, full.names = TRUE)
+  data_from_files <- 0
+  merge_my_data   <- 0
+  
+  # get the data frame and remove the NA values. 
+  my_file_data <- lapply(files_to_read[id], function(index){
+    
+    ## Read in the data in a frame
+    data_from_files <- read.csv(index, header = TRUE)
+    
+    ## pull out the NA values.
+    data_from_files <- na.omit(data_from_files)
+    
+    ## get our row count from the cleaned frame. 
+    number_of_rows  <- nrow(data_from_files)
+    
+    ## bind the two together....
+    my_data <-  cbind( basename(index), number_of_rows)
+    merged_data <- rbind(my_data)
+  
+    
+  })
+    
+  as.data.frame(my_file_data)
+  
+     
+ my_file_data
+  
+  
+  
+}
 
 pollutantmean <- function( directory, pollutant, id  = 1:332){
   ## directory is the path to the files.  
