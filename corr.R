1f33f41824d065f5ac134a216eab20325ba647ff
John Garcia <john.d.garcia01@gmail.com>
2016-05-22 11:46:56 -0500

    Added corr.R to the project.

---
+++ corr.R
@@ -0,0 +1,27 @@
+# Write a function that takes a directory of data files and a 
+# threshold for complete cases and calculates the correlation
+# between sulfate and nitrate for monitor locations where the
+# number of completely observed cases (on all variables) is 
+# greater than the threshold. The function should return a vector
+# of correlations for the monitors that meet the threshold requirement.
+# If no monitors meet the threshold requirement, then the function 
+# should return a numeric vector of length 0. A prototype of this 
+# function follows
+
+## Author John D. Garcia
+## Purpose: Find the correlation between to variables.  
+## We will use the complete.R function found in my Github
+## account.  It is a dependancy.  If you do not trust my 
+## complete.R function, then you should use complete.cases
+## R implementation.  You may have to perform some modifications 
+## to this code to get it to work properly.  Cheers!
+
+corr <- function( directory, threshold = 0){
+  ## directory is a character vector to the 
+  ## path where our files reside.  
+  
+  
+  
+  
+  
+}
